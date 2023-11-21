591b4ba9c13dfa2ebde9168306d5562065a9468a
uneeth-Gaikwad <puneethgaikwadl@gmail.com>
2023-11-21 17:45:14 +0530

    added suggestion

--- script.js
+++ script.js
@@ -2,23 +2,15 @@ const searchTxt = document.getElementById("search");
 const searchBox = document.getElementById("search-bar");
 const searchBtn = document.getElementById("search-btn");
 const form = document.getElementById("search-bar");
-
-const locationInput = document.getElementById("location");
-
-locationInput.addEventListener("keyup", function (event) {
-    console.log(event.target.value);
-});
-
-
-
+const date1 = document.getElementById("check-in");
+const date2 = document.getElementById("check-out");
+const Aincrement = document.getElementById("Aincrement");
+const Adecrement = document.getElementById("Adecrement");
 
 let Adults = 0;
 let children = 0;
 let infants = 0;
 let pets = 0;
-
-const Aincrement = document.getElementById("Aincrement");
-const Adecrement = document.getElementById("Adecrement");
 
 Adecrement.disabled = true;
 Adecrement.classList.add("disabled-btn");
@@ -235,9 +227,8 @@ Pdecrement.addEventListener("click", function () {
     Myvalue.innerText = newValue;
 });
 
-const date1 = document.getElementById("check-in");
-const date2 = document.getElementById("check-out");
 
+// disable the passed dates
 let date = new Date();
 let day = date.getDate();
 if (day < 10) {
@@ -252,20 +243,26 @@ let year = date.getFullYear();
 date1.min = `${year}-${month}-${day}`;
 date2.min = `${year}-${month}-${day}`;
 
+
+// this function is to animate search button (adding search text inside the button)
 searchBox.addEventListener("click", function () {
     searchTxt.innerText = "Search";
     searchBtn.classList.add("new-search");
 });
 
+// this function is to to animate search button (removing search text inside the button)
 document.addEventListener("click", function (event) {
     if (!document.getElementById("dropdown").contains(event.target)) {
-        if (!document.getElementById("search-bar").contains(event.target)) {
-            searchTxt.innerText = "";
-            searchBtn.classList.remove("new-search");
+        if (!document.getElementById("suggestions").contains(event.target)) {
+            if (!document.getElementById("search-bar").contains(event.target)) {
+                searchTxt.innerText = "";
+                searchBtn.classList.remove("new-search");
+            }
         }
     }
 });
 
+// this function displayes the drop down on focus
 const guestsInput = document.getElementById("guests");
 guestsInput.addEventListener("focus", function () {
     document.getElementById("dropdown").style.display = "flex";
@@ -273,6 +270,7 @@ guestsInput.addEventListener("focus", function () {
     searchBtn.classList.add("new-search");
 });
 
+// function to remove the dropdown if the user blur it
 document.addEventListener("click", function (event) {
     if (!document.getElementById("dropdown").contains(event.target)) {
         if (!document.getElementById("guests-container").contains(event.target)) {
@@ -281,10 +279,107 @@ document.addEventListener("click", function (event) {
     }
 });
 
+document.addEventListener("click", function (event) {
+    if (!document.getElementById("suggestions").contains(event.target)) {
+        if (!document.getElementById("location-container").contains(event.target)) {
+            document.getElementById("suggestions").style.display = "none";
+        }
+    }
+});
+
+const locationInput = document.getElementById("location");
+locationInput.addEventListener("keyup", function (event) {
+    if (event.target.value === "") {
+        document.getElementById("suggestions").style.display = "none";
+    } else {
+        document.getElementById("suggestions").style.display = "flex";
+        optimalfn(event);
+    }
+});
+
+const optimalfn = debounce(searchFun, 200);
+
+async function searchFun(event) {
+    console.log(event.target.value);
+    let payload = event.target.value;
+    const url = `https://airbnb13.p.rapidapi.com/autocomplete?query=${payload}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c5014a6b6emsh84288eb75db9f9fp1759f6jsn73c5e567590c',
+            'X-RapidAPI-Host': 'airbnb13.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        result.forEach(element => {
+            console.log(element.query);
+            createPlaceCards(element.query);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 
+function debounce(normalFn, delay) {
+    let timerId;
+    return function (...args) {
+        if (timerId) {
+            clearInterval(timerId);
+        }
+        timerId = setTimeout(() => {
+            placesContainer.innerHTML = '';
+            normalFn(...args);
+        }, delay);
+    }
+};
+
+
+const placesContainer = document.getElementById("places-container");
+function createPlaceCards(place) {
+    let placecard = document.createElement("div");
+    placecard.id = "locationName";
+    placecard.classList.add("place");
+    
+
+    let locationLogo = document.createElement("div");
+    locationLogo.classList.add("location-logo");
+    locationLogo.innerHTML = `<i class="fa-solid fa-location-dot"></i>`;
+
+    placecard.appendChild(locationLogo);
+
+    let placeName = document.createElement("p");
+    placeName.innerText = place;
+
+    placecard.appendChild(placeName);
+    placecard.addEventListener('click', function (e) {
+        console.log(e.target)
+    })
+
+    placesContainer.appendChild(placecard);
+}
+
+
+// extracting all the values in the search bar and creating an obj to send it to api
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    if (e.target.checkIn.value === "" || e.target.checkOut.value === "") {
+        console.error("Invalid date");
+        return;
+    }
+
+    let date1 = new Date(e.target.checkIn.value);
+    let date2 = new Date(e.target.checkOut.value);
+
+    console.log(date1.getTime(), date2.getTime());
+
+    if (date1.getTime() > date2.getTime()) {
+        console.error("Invalid checkOut");
+        return;
+    }
     const searchPlace = {
         place: e.target.place.value,
         checkIn: e.target.checkIn.value,
@@ -304,6 +399,7 @@ form.addEventListener("submit", (e) => {
     navigateTosearchListing();
 });
 
+// function to navigate to searchlisting.html page
 function navigateTosearchListing() {
     window.location.href = "searchListing.html";
 }
